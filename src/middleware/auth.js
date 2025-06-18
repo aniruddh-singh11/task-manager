501196191deab27f729ea274e0949c5ce8b1b68b
aniruddhs560@gmail.com <aniruddhs560@gmail.com>
2025-06-19 00:10:51 +0530

    =token check

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -9,6 +9,8 @@ const auth = async (req, res, next) => {
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
         const user = await User.findOne({ _id: decoded._id , 'tokens.token': token})
 
+        console.log(user)
+
         if(!user){
             throw new Error
         }
