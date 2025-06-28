26fbfbbb938f086791b8c48ff275b5c8d3afc077
aniruddhs560@gmail.com <aniruddhs560@gmail.com>
2025-06-28 23:18:06 +0530

    =Cors bug fix

--- src/app.js
+++ src/app.js
@@ -11,7 +11,10 @@ const taskRouter = require('./routers/task')
 const app = express()
 
 const cors = require('cors');
-app.use(cors());
+app.use(cors({
+  origin: 'https://task-manager-8wji.vercel.app',
+  credentials: true
+}));
 
 app.use(express.json())
 app.use(userRouter)
